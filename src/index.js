ee1a63049394cf4a7c061c04a845ee6f111811d6
HyunJoon <kimkuma04@gmail.com>
2020-08-05 16:34:07 +0900

    WatchView 프론트 처리

--- src/index.js
+++ src/index.js
@@ -34,23 +34,23 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
         <Route path="/" exact render={props => <WatchViewIndex {...props} />} />
-        <Route path="/index" exact render={props => <Index {...props} />} />
-      {/*<Route*/}
-      {/*  path="/landing-page"*/}
-      {/*  exact*/}
-      {/*  render={props => <Landing {...props} />}*/}
-      {/*/>*/}
-      {/*<Route path="/login-page" exact render={props => <Login {...props} />} />*/}
-      {/*<Route*/}
-      {/*  path="/profile-page"*/}
-      {/*  exact*/}
-      {/*  render={props => <Profile {...props} />}*/}
-      {/*/>*/}
-      {/*<Route*/}
-      {/*  path="/register-page"*/}
-      {/*  exact*/}
-      {/*  render={props => <Register {...props} />}*/}
-      {/*/>*/}
+        <Route path="/test" exact render={props => <Index {...props} />} />
+      <Route
+        path="/landing-page"
+        exact
+        render={props => <Landing {...props} />}
+      />
+      <Route path="/login-page" exact render={props => <Login {...props} />} />
+      <Route
+        path="/profile-page"
+        exact
+        render={props => <Profile {...props} />}
+      />
+      <Route
+        path="/register-page"
+        exact
+        render={props => <Register {...props} />}
+      />
       <Redirect to="/" />
     </Switch>
   </BrowserRouter>,
